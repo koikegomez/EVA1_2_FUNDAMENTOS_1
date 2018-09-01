e97da416f337fb49bb48f537598cac2f24b7c334
Jorge Alberto Gallegos Gomez <42745326+koikegomez@users.noreply.github.com>
2018-08-31 19:10:06 -0600

    Add files via upload

---
+++ EVA1_2_FUNDAMENTOS_1/src/JorgeGomez.java
@@ -0,0 +1,54 @@
+/*
+ * Indetificar elemntos basicos del lenguaje Java
+ */
+
+/** 
+ *
+ * @author Jorge Alerto Gallegos Gomez 18550721
+ */
+public class JorgeGomez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //TIPO DE DATO - BINBRE DE LA VARIABLE CONSTANTE IDENTIDICADOR
+      
+        int iEdad;//indetificador valido    
+        int _iEdad;// inicia cib _ identificador valido
+        int iEdad25;//numero no al inicio, identificador valido.
+        int iedad;// identificado valido.
+        int iEd$;// el signo de $ es valido solo en java.
+        int String; // No es recomendable, es un tipo de dato de Java.
+        int iValor=5+7;
+        
+        //-----------------------
+        // EJEMPLOS DE ERRORES DE IDENTIFICADORES.
+        int 25edad;
+        int tu edad;
+        int iEdad;
+        int algo-mas;// el - es un operador que reprecenta una resta aritmetica.
+        int int;
+        int float;
+     
+        
+        
+        
+    }
+    
+} 
+class Algo{
+
+}
+
+class Algo25{
+
+}
+
+class _AlgoMas{
+
+}
+
+class $algomas{}
+class cosas{}
